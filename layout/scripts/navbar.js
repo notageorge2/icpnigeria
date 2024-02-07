468b7a33e44fbcd7cdda519c4c80d5294948de8f
notageorge2 <notageorge2@mail.comgit config --global user.email>
2024-02-07 09:32:09 +0100

    hello

--- layout/scripts/navbar.js
+++ layout/scripts/navbar.js
@@ -1,5 +1,17 @@
-$('ul.nav li.dropdown').hover(function() {
-    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
-  }, function() {
-    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+// $('ul.nav li.dropdown').hover(function() {
+//     $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+//   }, function() {
+//     $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+//   });
+
+  $("#search-icon").click(function() {
+    $(".nav").toggleClass("search");
+    $(".nav").toggleClass("no-search");
+    $(".search-input").toggleClass("search-active");
   });
+  
+  $('.menu-toggle').click(function(){
+     $(".nav").toggleClass("mobile-nav");
+     $(this).toggleClass("is-active");
+  });
+  
